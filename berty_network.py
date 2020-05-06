7630385bb2d118b61c44d8b71f8b524d3dd03af9
mrbertp <aps795@gmail.com>
2020-05-06 03:11:03 +0200

    added batches

--- berty_network.py
+++ berty_network.py
@@ -1,14 +1,16 @@
 import numpy as np
 
 # 4 inputs
-# 1 batch
-inputs = [5, 3, 2, 4]
+# 3 batch
+inputs = [[5, 3, 2, 4],
+          [6, 2, 4, 3],
+          [7, 3, 2, 5]]
 
 # 3 neurons
 weights = [[0.02, 0.40, 0.10, 0.05],
            [0.20, 0.10, 0.04, 0.23],
            [0.34, 0.56, 0.02, 0.55]]
 
-outputs = np.dot(weights, inputs)
+outputs = np.dot(inputs, np.array(weights).T)
 
 print(outputs)
