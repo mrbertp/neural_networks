2a2b4798b78b913f790363c9c013460a62a8b7d6
mrbertp <aps795@gmail.com>
2020-05-10 15:53:52 +0200

    cleaned comments

--- berty_network.py
+++ berty_network.py
@@ -12,22 +12,14 @@ weights1 = [[0.02, 0.40, 0.10, 0.05],
             [0.34, 0.56, 0.02, 0.55]]
 
 # 2nd layer of 2 neurons
-# notice that the number of inputs has changed to 3, conditioned by dimensons of output1
+# number of inputs has changed to 3, conditioned by dimensons of output1
 weights2 = [[0.34, 0.56, 0.11],
             [0.22, 0.10, 0.12]]
-
-# the output of neurons computed via multiplication of matrixes 'inputs' and 'weights'
-# transposing weights for the dimensions to match
-
-# uno
-# dos
-# tres
 
 # calculating output of layer 1
 outputs1 = np.dot(inputs, np.array(weights1).T)
 
 # feeding output of layer 1 to layer 2 and calculating output of layer 2
-# uhg
 outputs2 = np.dot(outputs1, np.array(weights2).T)
 
 print(outputs2)
